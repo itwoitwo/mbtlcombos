4025cb5ff56ac1bfe76e66c8f750cc116d3a3b47
itwoitwo <moja84corns@gmail.com>
2021-09-24 06:46:46 +0900

    delete mbtlc_id column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('mbtlc_id');
-            $table->unsignedBigInteger('id')->unique()->index();
+            $table->unsignedBigInteger('id')->primary()->unique()->index();
             $table->string('id_name');
             $table->string('name');
             $table->string('main_character')->default('Unknown');
