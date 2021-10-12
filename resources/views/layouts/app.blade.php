d95de411c9ff17a815c8d5e1791b581b6a5de468
itwoitwo <moja84corns@gmail.com>
2021-10-13 07:33:55 +0900

    google ad auto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png">
         <link rel="manifest" href="/manifest.json">
         <script data-ad-client="ca-pub-8705074607112164" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8705074607112164"crossorigin="anonymous"></script>
         @stack('css')
     </head>
 
