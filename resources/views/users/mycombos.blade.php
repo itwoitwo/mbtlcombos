2e0ededb581bf3b17ff5edf26d98b28375b6a317
itwoitwo <moja84corns@gmail.com>
2021-10-13 16:01:55 +0900

    bottom ads

--- resources/views/users/mycombos.blade.php
+++ resources/views/users/mycombos.blade.php
@@ -18,6 +18,18 @@
         @include('combos.combos', ['combos' => $combos])
         <br>
         @include('combos.combo_annotation')
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8705074607112164"
+                crossorigin="anonymous"></script>
+        <!-- 横長 -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8705074607112164"
+            data-ad-slot="3369669378"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>               
     </div>
 </div>
 <br>
